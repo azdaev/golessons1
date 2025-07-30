181d7155b1e6cebf86212343041359c95a300d21
Amadi Azdaev <amady833@gmail.com>
2025-07-30 20:37:03 +0300

    add db postgresql

--- task1/handler/handler.go
+++ task1/handler/handler.go
@@ -1,11 +1,10 @@
 package handler
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"log"
 	"net/http"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
 )
 
 type Post struct {
@@ -20,42 +19,13 @@ type UpdatePostRequest struct {
 }
 
 type Handler struct {
-	LastID int
-	Posts  map[int]Post
+	db *pgx.Conn
 }
 
-func (h *Handler) GetPosts(c *gin.Context) {
-	posts := h.Posts
-	if len(posts) == 0 {
-		c.JSON(http.StatusNotFound, "Такого поста нет")
-		return
+func NewHandler(db *pgx.Conn) Handler {
+	return Handler{
+		db: db,
 	}
-
-	c.JSON(http.StatusOK, posts)
-}
-
-func (h *Handler) GetPostById(c *gin.Context) {
-	posts := h.Posts
-	if len(posts) == 0 {
-		c.JSON(http.StatusNotFound, "Такого поста нет")
-		return
-	}
-
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, "Невалидный id")
-		log.Println("error in getPostHandler in strconv.Atoi: ", err)
-		return
-	}
-
-	post, ok := h.Posts[id]
-	if !ok {
-		c.JSON(http.StatusNotFound, "Такого поста нет")
-		return
-	}
-
-	c.JSON(http.StatusOK, post)
 }
 
 func (h *Handler) CreatePost(c *gin.Context) {
@@ -66,60 +36,98 @@ func (h *Handler) CreatePost(c *gin.Context) {
 		return
 	}
 
-	h.LastID++
-	post.ID = h.LastID
-	h.Posts[h.LastID] = post
+	_, err = h.db.Exec(c, "insert into posts (title, body) values ($1, $2)", post.Title, post.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, "Что-то пошло не так")
+		log.Println("error insert into posts: ", err)
+	}
+
 	c.JSON(http.StatusOK, post)
 }
 
-func (h *Handler) DeletePost(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, "Невалидный id")
-		log.Println("error in getPostHandler in strconv.Atoi: ", err)
-		return
-	}
-
-	post, ok := h.Posts[id]
-	if !ok {
-		c.JSON(http.StatusNotFound, "Такого поста нет")
-		return
-	}
-
-	delete(h.Posts, post.ID)
-	c.JSON(http.StatusOK, "Пост успешно удален")
-}
-
-func (h *Handler) UpdatePost(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, "Невалидный id")
-		log.Println("error in getPostHandler in strconv.Atoi: ", err)
-		return
-	}
-
-	var updatePostRequest UpdatePostRequest
-	err = c.BindJSON(&updatePostRequest)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, "У вас невалидный запрос")
-		return
-	}
-
-	post, ok := h.Posts[id]
-	if !ok {
-		c.JSON(http.StatusNotFound, "Такого поста нет")
-		return
-	}
-
-	if updatePostRequest.Body != nil {
-		post.Body = *updatePostRequest.Body
-	}
-	if updatePostRequest.Title != nil {
-		post.Title = *updatePostRequest.Title
-	}
-	h.Posts[id] = post
-
-	c.JSON(http.StatusOK, h.Posts[id])
-}
+// h.db.QueryRow
+//func (h *Handler) GetPosts(c *gin.Context) {
+//	posts := h.Posts
+//	if len(posts) == 0 {
+//		c.JSON(http.StatusNotFound, "Такого поста нет")
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, posts)
+//}
+// h.db.Query
+//func (h *Handler) GetPostById(c *gin.Context) {
+//	posts := h.Posts
+//	if len(posts) == 0 {
+//		c.JSON(http.StatusNotFound, "Такого поста нет")
+//		return
+//	}
+//
+//	idStr := c.Param("id")
+//	id, err := strconv.Atoi(idStr)
+//	if err != nil {
+//		c.JSON(http.StatusBadRequest, "Невалидный id")
+//		log.Println("error in getPostHandler in strconv.Atoi: ", err)
+//		return
+//	}
+//
+//	post, ok := h.Posts[id]
+//	if !ok {
+//		c.JSON(http.StatusNotFound, "Такого поста нет")
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, post)
+//}
+// h.db.Exec
+//func (h *Handler) DeletePost(c *gin.Context) {
+//	idStr := c.Param("id")
+//	id, err := strconv.Atoi(idStr)
+//	if err != nil {
+//		c.JSON(http.StatusBadRequest, "Невалидный id")
+//		log.Println("error in getPostHandler in strconv.Atoi: ", err)
+//		return
+//	}
+//
+//	post, ok := h.Posts[id]
+//	if !ok {
+//		c.JSON(http.StatusNotFound, "Такого поста нет")
+//		return
+//	}
+//
+//	delete(h.Posts, post.ID)
+//	c.JSON(http.StatusOK, "Пост успешно удален")
+//}
+// h.db.Exec
+//func (h *Handler) UpdatePost(c *gin.Context) {
+//	idStr := c.Param("id")
+//	id, err := strconv.Atoi(idStr)
+//	if err != nil {
+//		c.JSON(http.StatusBadRequest, "Невалидный id")
+//		log.Println("error in getPostHandler in strconv.Atoi: ", err)
+//		return
+//	}
+//
+//	var updatePostRequest UpdatePostRequest
+//	err = c.BindJSON(&updatePostRequest)
+//	if err != nil {
+//		c.JSON(http.StatusBadRequest, "У вас невалидный запрос")
+//		return
+//	}
+//
+//	post, ok := h.Posts[id]
+//	if !ok {
+//		c.JSON(http.StatusNotFound, "Такого поста нет")
+//		return
+//	}
+//
+//	if updatePostRequest.Body != nil {
+//		post.Body = *updatePostRequest.Body
+//	}
+//	if updatePostRequest.Title != nil {
+//		post.Title = *updatePostRequest.Title
+//	}
+//	h.Posts[id] = post
+//
+//	c.JSON(http.StatusOK, h.Posts[id])
+//}
--- task1/main.go
+++ task1/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"networks/handler"
-
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"log"
+	"networks/handler"
 )
 
-type UpdatePostRequest struct {
-	Title *string `json:"title"`
-	Body  *string `json:"body"`
-}
-
 func main() {
-	r := gin.Default()
-
-	postsHandler := handler.Handler{
-		LastID: 0,
-		Posts:  make(map[int]handler.Post),
+	connString := "postgres://admin:admin@localhost:5432/mydb"
+	conn, err := pgx.Connect(context.Background(), connString)
+	if err != nil {
+		log.Fatal("Ошибка при подключении к БД: ", err)
 	}
 
+	r := gin.Default()
+
+	postsHandler := handler.NewHandler(conn)
+	
 	r.POST("/posts", postsHandler.CreatePost)
-	r.GET("/posts/:id", postsHandler.GetPostById)
-	r.GET("/posts", postsHandler.GetPosts)
-	r.DELETE("/posts/:id", postsHandler.DeletePost)
-	r.PUT("/posts/:id", postsHandler.UpdatePost)
+	//r.GET("/posts/:id", postsHandler.GetPostById)
+	//r.GET("/posts", postsHandler.GetPosts)
+	//r.DELETE("/posts/:id", postsHandler.DeletePost)
+	//r.PUT("/posts/:id", postsHandler.UpdatePost)
 
 	r.Run(":8085")
 }
